899112bcb720342eaa5acb880730acc330badd17
fwdAndroid <fwdkaleem@gmail.com>
2024-12-29 12:30:14 +0500

    Booking Complete

---
+++ lib/detail/parking_detail.dart
@@ -0,0 +1,46 @@
+
+import 'package:carpark/utils/colors.dart';
+import 'package:carpark/utils/message_utils.dart';
+import 'package:carpark/widgets/buttons.dart';
+import 'package:flutter/material.dart';
+
+class ParkingDetail extends StatefulWidget {
+  String name;
+  var price;
+  var flag;
+   ParkingDetail({super.key,required this.name,required this.price,required this.flag});
+
+  @override
+  State<ParkingDetail> createState() => _ParkingDetailState();
+}
+
+class _ParkingDetailState extends State<ParkingDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Padding(
+
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text("Parking Space Name: ",style: TextStyle(color: black,fontSize: 22,fontWeight: FontWeight.bold),
+                ),Text(widget.name,style: TextStyle(color: black,fontSize: 16,fontWeight: FontWeight.w600),)],
+            ),
+            Row(
+              children: [
+                Text("Price Per Hr: ",style: TextStyle(color: black,fontSize: 22,fontWeight: FontWeight.bold),
+                ),Text(widget.price.toString(),style: TextStyle(color: black,fontSize: 16,fontWeight: FontWeight.w600),)],
+            ),
+            SaveButton(title: "Book", onTap: (){
+              showMessageBar("Booking Complete", context);
+              Navigator.pop(context);
+            }, color: mainBtnColor)
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/pages/parking_page.dart
+++ lib/screens/pages/parking_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
+import '../../detail/parking_detail.dart';
 
 class ParkingPage extends StatefulWidget {
   const ParkingPage({super.key});
@@ -10,6 +13,49 @@ class ParkingPage extends StatefulWidget {
 class _ParkingPageState extends State<ParkingPage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: StreamBuilder(
+          stream: FirebaseFirestore.instance.collection("parking").snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if (snapshot.data!.docs.isEmpty) {
+              return Center(
+                child: Text(
+                  "No Parking Reported Yet",
+                  style: TextStyle(color: Colors.black),
+                ),
+              );
+            }
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final List<DocumentSnapshot> documents = snapshot.data!.docs;
+                  final Map<String, dynamic> data =
+                  documents[index].data() as Map<String, dynamic>;
+                  return GestureDetector(
+                    onTap: () {},
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Card(
+                          child: ListTile(
+                            trailing: TextButton(onPressed: (){
+                              Navigator.push(context, MaterialPageRoute(builder: (builder) => ParkingDetail(
+                                name: data['name'],
+                                price: data['price'],
+
+                                flag: data['isAvaialble'],
+                              )));                            }, child: Text("Booking")),
+                            title: Text(data['name']),
+                            subtitle: Text(data['price'].toString()),
+                          )),
+                    ),
+                  );
+                });
+          }),
+    );
   }
 }
